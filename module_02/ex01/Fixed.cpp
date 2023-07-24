078ee64b2369d396f6b33ba996229767709d60ea
Alan BOULESTEIX <alan.boulesteix@gmail.com>
2023-07-24 19:25:25 +0200

    Start Module 02 ex 01

--- module_02/ex01/Fixed.cpp
+++ module_02/ex01/Fixed.cpp
@@ -6,7 +6,33 @@
 /*   By: aboulest <aboulest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 18:32:29 by aboulest          #+#    #+#             */
-/*   Updated: 2023/07/24 18:32:30 by aboulest         ###   ########.fr       */
+/*   Updated: 2023/07/24 19:24:45 by aboulest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+// std::ostream& operator<<(std::ostream & o, Fixed const & fixed){
+// 	o << 
+// };
+
+Fixed::Fixed(void):_n(0){
+	std::cout << "Default constructor called" << std::endl;
+};
+
+Fixed::Fixed(const Fixed &fixed){
+	std::cout << "Copy constructor called" << std::endl;
+	*this = fixed;
+};
+
+Fixed::Fixed(const int integer){
+	std::cout << "Integer constructor called" << std::endl;
+};
+
+Fixed::Fixed(const float flo){
+	std::cout << "Float constructor called" << std::endl;
+};
+
+Fixed::~Fixed(void){
+	std::cout << "Destructor called" << std::endl;
+};
--- module_02/ex01/Fixed.hpp
+++ module_02/ex01/Fixed.hpp
@@ -6,12 +6,14 @@
 /*   By: aboulest <aboulest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 18:32:34 by aboulest          #+#    #+#             */
-/*   Updated: 2023/07/24 18:40:30 by aboulest         ###   ########.fr       */
+/*   Updated: 2023/07/24 19:16:26 by aboulest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
+
+#include <iostream>
 
 class Fixed{
 
@@ -30,12 +32,16 @@ class Fixed{
 	//Functions
 	int		getRawBits( void ) const;
 	void	setRawBits( int const raw );
-	
+	float	toFloat( void ) const;
+	int		toInt( void ) const;
+
 	private:
 	
 	//Attributs
 	int	_n;
 	int	_nBits = 8;
 };
+
+std::ostream& operator<<(std::ostream & o, Fixed const & fixed);
 
 #endif
