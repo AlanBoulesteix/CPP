01cc67fc88e9d7056a9181aa69b8d4206b5c9338
Alan BOULESTEIX <alan.boulesteix@gmail.com>
2023-07-24 14:32:31 +0200

    Finish module 01/ex04

---
+++ module_01/ex04/Replace.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboulest <aboulest@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 13:54:54 by aboulest          #+#    #+#             */
+/*   Updated: 2023/07/24 14:31:45 by aboulest         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REPLACE_HPP
+# define REPLACE_HPP
+
+#include <ostream>
+#include <iostream>
+#include <fstream>
+
+# define ARG 1
+# define EMPTY 2
+# define INFILE 3
+# define OUTFILE 4
+
+#endif
--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -6,34 +6,70 @@
 /*   By: aboulest <aboulest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 19:17:22 by aboulest          #+#    #+#             */
-/*   Updated: 2023/07/20 19:48:50 by aboulest         ###   ########.fr       */
+/*   Updated: 2023/07/24 14:31:42 by aboulest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Replace.hpp"
-#include <ostream>
-#include <iostream>
-#include <fstream>
+
+int	errorManager(int mode){
+
+	std::cerr << "\033[1;31mError:\033[0m" << std::endl;
+	if (mode == ARG)
+		std::cerr << "Usage: ./replace <file> <holder> <substitute>" << std::endl;
+	else if (mode == EMPTY)
+		std::cerr << "Holder can not be empty" << std::endl;
+	else if (mode == INFILE)
+		std::cerr << "Something went wrong with input file" << std::endl;
+	else if (mode == OUTFILE)
+		std::cerr << "Something went wrong with output file" << std::endl;
+	return (1);
+};
+
+void	readFile(std::ifstream	*file, std::string *content){
+	char	c;
+	
+	while ((c = (*file).get()) != EOF)
+		*content += c;
+};
+
+void	writeAndReplace(std::ofstream *outfile, std::string content, 
+	std::string holder, std::string substitute){
+	size_t index = content.find(holder);
+	int		i = 0;
+	while(index != std::string::npos)
+	{
+		(*outfile) << content.substr(i, index - i);
+		(*outfile) << substitute;
+		i = index + holder.length();
+		index = content.find(holder, index + holder.length());
+	}
+	(*outfile) << content.substr(i, content.length() - i);
+};
 
 int main(int ac, char **av)
 {
 	std::ifstream	file;
-	std::string		buf;
+	std::ofstream	outfile;
+	std::string		holder;
+	std::string		substitute;
+	std::string		infileName;
+	std::string		content;
 	
-	(void)av;
 	if (ac != 4)
-		std::cout << "Error" << std::endl
-				<< "Wrong amount of arguments" << std::endl;
-	file.open(av[1]);
+		return (errorManager(ARG));
+	holder = av[2];
+	substitute = av[3];
+	infileName = av[1];
+	file.open(infileName.c_str());
+	if (holder.empty())
+		return (errorManager(EMPTY));
 	if (!file.is_open())
-	{
-		std::cout << "Error" << std::endl
-				<< "Something went wrong with input file" << std::endl;
-		return (1);
-	}
-	while (!file.eof())
-	{
-		std::getline(file, buf);
-		std::cout << buf << std::endl;
-	}
+		return (errorManager(INFILE));
+	outfile.open((infileName + ".replace").c_str());
+	if (!file.is_open())
+		return (errorManager(OUTFILE));
+	readFile(&file, &content);
+	writeAndReplace(&outfile, content, holder, substitute);
+	file.close();
 }
