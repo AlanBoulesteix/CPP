dba4a4f84e7667e35b24b160a131a7cddf9eea1b
Alan BOULESTEIX <alan.boulesteix@gmail.com>
2023-07-27 16:46:49 +0200

    module 02 / Ex03 changed

--- module_02/ex03/bsp.cpp
+++ module_02/ex03/bsp.cpp
@@ -6,19 +6,35 @@
 /*   By: aboulest <aboulest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 17:48:51 by aboulest          #+#    #+#             */
-/*   Updated: 2023/07/26 18:57:06 by aboulest         ###   ########.fr       */
+/*   Updated: 2023/07/27 15:18:34 by aboulest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-bool bsp( Point const a, Point const b, Point const c, Point const point){
-	Point ab((b.getX() - a.getX()), (b.getY() - b.getY()));
-	Point ac((c.getX() - a.getX()), (c.getY() - a.getY())); 
-	Point ap((point.getX() - a.getX()), (point.getY() - a.getY()));
-	Fixed apab((ap.getX())*(ab.getX()) + (ap.getY())*(ab.getY()));
-	Fixed apac((ap.getX())*(ac.getX()) + (ap.getY())*(ac.getY()));
-	
-	std::cout << "APAB= " << apab << " APAC= " << apac << std::endl;
-	return (apab != 0 && apac != 0);
+Fixed	abs(Fixed nb)
+{
+	return ((nb < 0 ? nb * (-1) : nb));
+}
+
+Fixed	area( Point const p1, Point const p2, Point const p3){
+	Fixed a;
+
+	a = (p1.getX()*(p2.getY()-p3.getY())) + (p2.getX()*(p3.getY()-p1.getY())) + (p3.getX()*(p1.getY() - p2.getY()));
+	return (abs(a)/2);
 };
+
+bool bsp( Point const a, Point const b, Point const c, Point const point){
+
+	Fixed abc, abpoint, acpoint, bcpoint;
+		
+	abc = area(a, b, c);
+	abpoint = area(a, b, point);
+	acpoint = area(a, c, point);
+	bcpoint = area(b, c, point);
+	if (abc == 0 || abpoint == 0 || acpoint == 0 || bcpoint == 0)
+		return (false);
+	if (abpoint + acpoint + bcpoint == abc)
+		return (true);
+	return (false);
+}; 
--- module_02/ex03/main.cpp
+++ module_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aboulest <aboulest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 18:17:47 by aboulest          #+#    #+#             */
-/*   Updated: 2023/07/26 18:53:22 by aboulest         ###   ########.fr       */
+/*   Updated: 2023/07/27 15:22:36 by aboulest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,26 @@ void	exempleKo(void){
 	std::cout << (bsp(a, b, c, point) ? "Oui" : "Non") << std::endl;
 };
 
+void	exempleOk2(void){
+	Point a(4,4), b(6,1), c(2,1), point(4,2);
+	std::cout << "Le point" << point
+			<< " est il dans le triangle ayant pour sommet :" << std::endl
+			<< "A" << a << " B" << b << " C" << c << " ?" << std::endl;
+	std::cout << (bsp(a, b, c, point) ? "Oui" : "Non") << std::endl;
+};
+
+void	exempleOk3(void){
+	Point a(4,4), b(6,1), c(2,1), point(4,4);
+	std::cout << "Le point" << point
+			<< " est il dans le triangle ayant pour sommet :" << std::endl
+			<< "A" << a << " B" << b << " C" << c << " ?" << std::endl;
+	std::cout << (bsp(a, b, c, point) ? "Oui" : "Non") << std::endl;
+};
+
 int main(void)
 {
 	exempleOk();
 	exempleKo();
+	exempleOk2();
+	exempleOk3();
 }
