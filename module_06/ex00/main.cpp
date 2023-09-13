6326d2fdcac1a253ee450714eae6aaabc4826011
Alan BOULESTEIX <alan.boulesteix@gmail.com>
2023-09-13 17:41:42 +0200

    finish convert int

--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alanboulesteix <alanboulesteix@student.    +#+  +:+       +#+        */
+/*   By: aboulest <aboulest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 14:52:34 by aboulest          #+#    #+#             */
-/*   Updated: 2023/09/12 18:38:23 by alanboulest      ###   ########.fr       */
+/*   Updated: 2023/09/13 17:34:55 by aboulest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,19 @@
 int main()
 {
 	std::string	input;
+	
 	while (1)
 	{
-		std::cin >> input;
-		ScalarConverter::convert(input);	
+		std::cout << "Enter input: " ;
+		std::getline(std::cin,input);
+		if (std::cin.eof())
+		{
+			std::cout << std::endl << "CTL-D received" << std::endl 
+				<< "Programe will close" << std::endl;
+			return (0);
+		}
+		ScalarConverter::convert(input);
+		std::cout << "================" << std::endl;
 	}
 	return (0);
 }
--- module_06/ex00/utils.cpp
+++ module_06/ex00/utils.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alanboulesteix <alanboulesteix@student.    +#+  +:+       +#+        */
+/*   By: aboulest <aboulest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 13:33:32 by aboulest          #+#    #+#             */
-/*   Updated: 2023/09/12 21:37:57 by alanboulest      ###   ########.fr       */
+/*   Updated: 2023/09/13 17:35:18 by aboulest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,16 +80,24 @@ void	convertChar(std::string str)
 	int		intValue = static_cast<int>(c) ;
 	float	floatValue = static_cast<float>(intValue);
 	double	doubleValue = static_cast<double>(intValue);
-	std::cout <<"Initial value = " << str << std::endl;
+	std::cout << "Initial value = " << str << std::endl;
+	std::cout << "Char convertion = " << c << std::endl;
 	std::cout << "int convertion = " << intValue << std::endl;
 	std::cout << "Float convertion = " << floatValue << std::endl;
 	std::cout << "Double convertion = " << doubleValue << std::endl;
-	std::cout << "Char convertion = " << c << std::endl;
 }
 
 void	convertInt(std::string str)
 {
-	std::cout <<"Initial value = " << str << std::endl;
+	int		intValue = atoi(str.c_str());
+	float	floatValue = static_cast<float>(intValue);
+	double	doubleValue = static_cast<double>(intValue);
+	char	c = static_cast<char>(intValue);
+	std::cout << "Initial value = " << str << std::endl;
+	std::cout << "Char convertion = " << c << std::endl;
+	std::cout << "int convertion = " << intValue << std::endl;
+	std::cout << "Float convertion = " << floatValue << ".0f" << std::endl;
+	std::cout << "Double convertion = " << doubleValue << ".0" << std::endl;
 }
 
 void	convertFloat(std::string str)
@@ -101,10 +109,4 @@ void	convertFloat(std::string str)
 void	convertDouble(std::string str)
 {
 	(void)str;
-}
-
-void	printAllConvertion(char charV, int intV, float floatV, double doubleV)
-{
-	std::string	cStr(1, charV);
-	std::string	iStr(intV);
 }
