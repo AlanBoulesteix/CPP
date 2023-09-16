7af4485ee0ef0a0787f9869f014112370996ead4
AlanBoulesteix <alan.boulesteix2312@gmail.com>
2023-09-16 16:17:54 +0200

    try & catch used

--- module_06/ex01/main.cpp
+++ module_06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alanboulesteix <alanboulesteix@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 14:52:34 by aboulest          #+#    #+#             */
-/*   Updated: 2023/09/16 16:13:34 by alanboulest      ###   ########.fr       */
+/*   Updated: 2023/09/16 16:16:05 by alanboulest      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,15 @@ int main()
 {
     Data    *a = new Data("Mass Structure ", 42, true);
     std::cout << *a << std::endl;
-    uintptr_t rawptr = Serializer::serialize(a);
-    std::cout << "============================" << std::endl;
-    Data *b = Serializer::deserialize(rawptr);
-    std::cout << *b << std::endl;
+    try {
+        uintptr_t rawptr = Serializer::serialize(a);
+        std::cout << "============================" << std::endl;
+        Data *b = Serializer::deserialize(rawptr);
+        std::cout << *b << std::endl;
+    }
+    catch (std::exception &e){
+        std::cout << e.what() << std::endl;
+    }
     delete a;
     return (0);
 
